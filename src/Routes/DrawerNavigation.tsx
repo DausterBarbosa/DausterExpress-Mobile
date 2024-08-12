7460fbabcc2f60069d82130f80adcefbbc61b1ac
Dauster Barbosa <dausterbarbosa12@gmail.com>
2024-08-12 16:35:01 -0300

    integração com push notification

--- src/Routes/DrawerNavigation.tsx
+++ src/Routes/DrawerNavigation.tsx
@@ -1,18 +1,50 @@
+import {useEffect} from "react";
+
 import {createDrawerNavigator} from "@react-navigation/drawer";
 
 import HeaderBar from "../components/HeaderBar";
 import SideBar from "../components/SideBar";
 
 import Dashboard from "../pages/Dashboard";
-import ChangePassword from "../pages/ChangePassword";
 import SupportChat from "../pages/SupportChat";
 
 import DeliveriesStackNavigation from "./DeliveriesStackNavigation";
 import SettingsStackNavigation from "./SettingsStackNavigation";
 
+import messaging from '@react-native-firebase/messaging';
+
+import notifee from "@notifee/react-native";
+
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigation(){
+    useEffect(() => {
+        async function handleNotifications(){
+            const channelId = await notifee.createChannel({
+                id: 'default',
+                name: 'Default Channel',
+                sound: 'default',
+                vibration: true,
+              });
+
+            const unsubscribe = messaging().onMessage(async remoteMessage => {
+                notifee.displayNotification({title: "asdf", body: "asdf", android:{channelId}});
+            });
+        
+            // messaging().onNotificationOpenedApp(async remoteMessage => {
+            //   console.log("App fodendo");
+            // });
+        
+            // messaging().getInitialNotification().then(remoteMessage => {
+            //   console.log("App fodendo");
+            // });
+        
+            return unsubscribe;
+        }
+
+        handleNotifications();
+    }, []);
+
     return (
         <Drawer.Navigator
             drawerContent={() => <SideBar/>}
